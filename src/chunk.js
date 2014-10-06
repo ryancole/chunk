66cca808eff694d72a8527b92be470bee9bfea93
Ryan Cole <ryan@rycole.com>
2014-10-05 23:43:27 -0500

    add tests, add node / browser support

--- src/chunk.js
+++ src/chunk.js
@@ -1,21 +1,36 @@
+"use strict";
 
-module.exports = function chunk (collection, size) {
+(function () {
+
+    function chunk (collection, size) {
     
-    var result = [];
-    
-    // default size to two item
-    size = parseInt(size) || 2;
-    
-    // add each chunk to the result
-    for (var x = 0; x < Math.ceil(collection.length / size); x++) {
+        var result = [];
         
-        var start = x * size,
-            end = start + size;
+        // default size to two item
+        size = parseInt(size) || 2;
         
-        result.push(collection.slice(start, end));
+        // add each chunk to the result
+        for (var x = 0; x < Math.ceil(collection.length / size); x++) {
+            
+            var start = x * size;
+            var end = start + size;
+            
+            result.push(collection.slice(start, end));
+            
+        }
         
+        return result;
+        
+    };
+
+    // export in node or browser
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = chunk;
+        }
+        exports.chunk = chunk;
+    } else {
+        this.chunk = chunk;
     }
-    
-    return result;
-    
-};
+
+}.call(this));
